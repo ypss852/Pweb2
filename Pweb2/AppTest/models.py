1e893a7fa2453b7ca4ca1c17b2c6d81858549981
Sadith <herrerataty433@gmail.com>
2024-11-26 13:51:39 -0800

    Creacion de modelo Usuario, bajo los parametros de AbstractUser

--- Pweb2/AppTest/models.py
+++ Pweb2/AppTest/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class Usuario(AbstractUser):
+    bio = models.TextField(blank=True, null=True)  # Biografía opcional
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)  # Imagen de perfil
+
+    def _str_(self):
+        return self.username
+    
