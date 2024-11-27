7561ac93b1c3ece380917d74f53cfa350d223659
ypss852 <falvarezlla@unsa.edu.pe>
2024-11-26 21:28:37 -0500

    Subiendo los dos models restantes

--- AppTest/models.py
+++ AppTest/models.py
@@ -18,3 +18,20 @@ class Libro(models.Model):
 
     def _str_(self):
         return f"{self.titulo} de {self.autor}"
+    
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Reseña(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='reseñas')
+    libro = models.ForeignKey(Libro, on_delete=models.CASCADE, related_name='reseñas')
+    comentario = models.TextField()
+    calificacion = models.PositiveSmallIntegerField()  
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.usuario.username} - {self.libro.titulo}"
